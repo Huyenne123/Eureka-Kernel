794aaad01293c8728586e00985b461f3bc84b1b3
Huyenne123 <87600660+Huyenne123@users.noreply.github.com>
2026-03-28 20:13:21 +0700

    Update arraymap.c

--- kernel/bpf/arraymap.c
+++ kernel/bpf/arraymap.c
@@ -428,7 +428,7 @@ static void prog_fd_array_put_ptr(void *ptr)
 {
 	struct bpf_prog *prog = ptr;
 
-	bpf_prog_put_rcu(prog);
+	bpf_prog_put(prog);
 }
 
 /* decrement refcnt of all bpf_progs that are stored in this map */
