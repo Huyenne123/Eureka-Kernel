63fccb313b38b3f554202fbe5c586289e14360a9
Huyenne123 <87600660+Huyenne123@users.noreply.github.com>
2026-03-29 14:41:57 +0700

    Check fragsize against IPV4_MIN_MTU

--- net/ipv4/ip_output.c
+++ net/ipv4/ip_output.c
@@ -1184,7 +1184,7 @@ static int ip_setup_cork(struct sock *sk, struct inet_cork *cork,
 	cork->fragsize = ip_sk_use_pmtu(sk) ?
 			 dst_mtu(&rt->dst) : READ_ONCE(rt->dst.dev->mtu);
 
-	if (!inetdev_valid_mtu(cork->fragsize))
+	if (cork->fragsize < IPV4_MIN_MTU)
 		return -ENETUNREACH;
 
 	cork->dst = &rt->dst;
