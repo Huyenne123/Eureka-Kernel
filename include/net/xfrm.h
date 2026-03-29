4d9f76dd94dbbbd22ece5830bd2f9477fcd62392
Huyenne123 <87600660+Huyenne123@users.noreply.github.com>
2026-03-29 15:48:52 +0700

    Add xfrm_id_proto_valid function
    Add inline function to validate IP protocol types.

--- include/net/xfrm.h
+++ include/net/xfrm.h
@@ -1313,7 +1313,13 @@ static inline int xfrm_id_proto_match(u8 proto, u8 userproto)
 						  proto == IPPROTO_ESP ||
 						  proto == IPPROTO_COMP)));
 }
-
+static inline int xfrm_id_proto_valid(u8 proto)
+{
+    return proto == IPPROTO_AH ||
+           proto == IPPROTO_ESP ||
+           proto == IPPROTO_COMP ||
+           proto == IPSEC_PROTO_ANY;
+}
 /*
  * xfrm algorithm information
  */
