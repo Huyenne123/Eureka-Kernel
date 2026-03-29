1c8cd59bce429745e9a61caa943dc8043cf5381b
Huyenne123 <87600660+Huyenne123@users.noreply.github.com>
2026-03-29 15:00:00 +0700

    Update addrconf.h

--- include/net/addrconf.h
+++ include/net/addrconf.h
@@ -167,6 +167,8 @@ int ipv6_sock_mc_join(struct sock *sk, int ifindex,
 int ipv6_sock_mc_drop(struct sock *sk, int ifindex,
 		      const struct in6_addr *addr);
 void ipv6_sock_mc_close(struct sock *sk);
+void __ipv6_sock_mc_close(struct sock *sk);
+void __ipv6_sock_ac_close(struct sock *sk);
 bool inet6_mc_check(struct sock *sk, const struct in6_addr *mc_addr,
 		    const struct in6_addr *src_addr);
 
